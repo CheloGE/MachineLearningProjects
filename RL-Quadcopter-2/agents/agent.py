07947836b8289035238b0d78c55f1266f8a18cf1
Ubuntu <ubuntu@ip-172-31-44-233.us-east-2.compute.internal>
2018-07-23 17:43:31 +0000

    test

--- RL-Quadcopter-2/agents/agent.py
+++ RL-Quadcopter-2/agents/agent.py
@@ -40,7 +40,7 @@ class DDPG():
 
         # Replay memory
         self.buffer_size = 100000
-        self.batch_size = 64
+        self.batch_size = 128#64
         self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
 
         # Algorithm parameters
@@ -151,9 +151,12 @@ class Actor:
         states = layers.Input(shape=(self.state_size,), name='states')
 
         # Add hidden layers
-        net = layers.Dense(units=32, activation='relu')(states)
-        net = layers.Dense(units=64, activation='relu')(net)
-        net = layers.Dense(units=32, activation='relu')(net)
+        #net = layers.Dense(units=32, activation='relu')(states)
+        #net = layers.Dense(units=64, activation='relu')(net)
+        #net = layers.Dense(units=32, activation='relu')(net)
+        net = layers.Dense(units=300, activation='relu')(states)
+        net = layers.Dense(units=400, activation='relu')(net)
+        net = layers.Dense(units=200, activation='relu')(net)
 
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
@@ -207,12 +210,16 @@ class Critic:
         actions = layers.Input(shape=(self.action_size,), name='actions')
 
         # Add hidden layer(s) for state pathway
-        net_states = layers.Dense(units=32, activation='relu')(states)
-        net_states = layers.Dense(units=64, activation='relu')(net_states)
+        #net_states = layers.Dense(units=32, activation='relu')(states)
+        #net_states = layers.Dense(units=64, activation='relu')(net_states)
+        net_states = layers.Dense(units=300, activation='relu')(states)
+        net_states = layers.Dense(units=400, activation='relu')(net_states)
 
         # Add hidden layer(s) for action pathway
-        net_actions = layers.Dense(units=32, activation='relu')(actions)
-        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+        #net_actions = layers.Dense(units=32, activation='relu')(actions)
+        #net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+        net_actions = layers.Dense(units=300, activation='relu')(actions)
+        net_actions = layers.Dense(units=400, activation='relu')(net_actions)
 
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
